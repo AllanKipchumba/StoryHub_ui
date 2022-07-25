667798c702e2b457332b5a11b557b291b166fec1
_Allan <allankipchumba76@gmail.com>
2022-07-25 06:18:11 +0300

    Transfered root reducer logic to store.js

--- src/Redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import loginReducer from "./slices/loginSlice";
-import signupReducer from "./slices/signupSlice";
-
-export const rootReducer = combineReducers({
-    logIn: loginReducer,
-    signUp: signupReducer,
-});
