47ce2deb919fbb9749d95b9814d80390b5326a1a
_Allan <allankipchumba76@gmail.com>
2022-10-04 15:18:39 +0300

    removed select category in posts

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -13,26 +13,10 @@ const override = {
 };
 
 export const Posts = () => {
-  const [category, setCategory] = useState("");
   const { loading } = useSelector((store) => store["logIn"]);
   const dispatch = useDispatch();
   const { search } = useLocation();
   const [posts, setPosts] = useState([]);
-
-  const handleChange = (e) => {
-    setCategory(e.target.value);
-  };
-
-  useEffect(() => {
-    if (!category) {
-    } else {
-      try {
-        window.location.replace(`/?cat=${category}`);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }, [category]);
 
   //FETCH POSTS FROM DB
   useEffect(() => {
@@ -43,7 +27,6 @@ export const Posts = () => {
         const res = await axios.get(
           "http://localhost:5000/api/posts/" + search
         );
-        console.log(res.data.length);
         setPosts(res.data);
         dispatch(loadingStop());
       };
@@ -71,17 +54,6 @@ export const Posts = () => {
               <h1 className="md:px-16 font-bold tracking-[2.72px] text-[16px] capitalize">
                 <span>latest</span> stories
               </h1>
-              <div className="input" id="category">
-                <select onChange={handleChange}>
-                  <option>select category</option>
-                  <option value="education">Education</option>
-                  <option value="news">News</option>
-                  <option value="health">Health</option>
-                  <option value="engineering">Engineering</option>
-                  <option value="sports">Sports</option>
-                  <option value="other">Other</option>
-                </select>
-              </div>
             </div>
 
             <div className=" py-3 m-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
